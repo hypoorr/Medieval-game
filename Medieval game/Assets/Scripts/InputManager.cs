da2ffab412f5dbaf972a911338530c616ca1f7d6
hypoorr <ollie.heron@gmail.com>
2026-01-06 09:50:58 +0000

    player

---
+++ Medieval game/Assets/Scripts/InputManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    public PlayerInput.OnFootActions onFoot;
+
+    private PlayerMotor motor;
+    private PlayerLook look;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+        onFoot = playerInput.OnFoot;
+        motor = GetComponent<PlayerMotor>();
+        look = GetComponent<PlayerLook>();
+        onFoot.Jump.performed += ctx => motor.Jump();
+
+    }
+
+    void FixedUpdate()
+    {
+        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+    }
+
+    private void LateUpdate()
+    {
+        look.ProcessLook(onFoot.Look.ReadValue<Vector2>());
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnEnable()
+    {
+        onFoot.Enable();
+    }
+
+    private void OnDisable()
+    {
+        onFoot.Disable();
+    }
+}
