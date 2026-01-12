ae2c1e8311d5a96851673060f65623bffd8128b7
hypoorr <ollie.heron@gmail.com>
2026-01-12 11:19:28 +0000

    um

--- Medieval game/Assets/Scripts/Player/PlayerMotor.cs
+++ Medieval game/Assets/Scripts/Player/PlayerMotor.cs
@@ -5,6 +5,17 @@ using UnityEngine.InputSystem;
 
 public class PlayerMotor : MonoBehaviour
 {
+<<<<<<< Updated upstream
+=======
+
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    public float speed = 5f;
+    private bool isGrounded;
+    public float gravity = -9.8f;
+    public float jumpHeight = 1.5f;
+    public bool isMoving = false;
+>>>>>>> Stashed changes
     PlayerInput playerInput;
     PlayerInput.OnFootActions input;
 
@@ -63,12 +74,22 @@ public class PlayerMotor : MonoBehaviour
         Vector3 moveDirection = Vector3.zero;
         moveDirection.x = input.x;
         moveDirection.z = input.y;
+        isMoving = true;
 
+<<<<<<< Updated upstream
         controller.Move(transform.TransformDirection(moveDirection) * moveSpeed * Time.deltaTime);
         _PlayerVelocity.y += gravity * Time.deltaTime;
         if (isGrounded && _PlayerVelocity.y < 0)
             _PlayerVelocity.y = -2f;
         controller.Move(_PlayerVelocity * Time.deltaTime);
+=======
+        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+        playerVelocity.y += gravity * Time.deltaTime;
+        if(isGrounded && playerVelocity.y < 0)
+            playerVelocity.y = -2f;
+        controller.Move(playerVelocity * Time.deltaTime);
+        isMoving = false;
+>>>>>>> Stashed changes
     }
 
     void LookInput(Vector3 input) // turns camera 
