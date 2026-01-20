381c021e5fc280d064a0e6fc2e3e81d16edf6105
hypoorr <ollie.heron@gmail.com>
2026-01-20 12:58:25 +0000

    level setup

---
+++ Medieval game/Assets/Scripts/LevelSetup.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+public class LevelSetup : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private GameObject targetObject;
+
+    [Header("Level settings")]
+    [SerializeField]
+    private int enemyCount;
+
+
+
+
+    //Setup enemy spawns
+
+
+    
+
+    void Start()
+    {
+
+        // Get cube bounds
+        Renderer rend = targetObject.GetComponent<Renderer>();
+
+        Bounds bounds = rend.bounds;
+
+        for(int i = 0; i < enemyCount; i++)
+        {
+            Vector3 point = Vector3.zero;
+            point = new Vector3(Random.Range(bounds.min.x, bounds.max.x), bounds.max.y, Random.Range(bounds.min.z, bounds.max.z));
+            gameObject.transform.position = point;
+            GameObject enemy = Instantiate(enemyPrefab, gameObject.transform);
+            enemy.transform.SetParent(null, true);
+        }
+
+        
+    }
+}
